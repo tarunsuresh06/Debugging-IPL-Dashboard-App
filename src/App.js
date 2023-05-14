51edb47c49a7ed23fcc63393fae490e6b765c58c
tommy <tarunsuresh.v@gmail.com>
2023-05-14 13:05:55 +0000

    Fixed Bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 import Home from './components/Home'
 import NotFound from './components/NotFound'
@@ -7,13 +7,11 @@ import TeamMatches from './components/TeamMatches'
 import './App.css'
 
 const App = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/team-matches/:id" component={TeamMatches} />
-      <Route component={NotFound} />
-    </Switch>
-  </BrowserRouter>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/team-matches/:id" component={TeamMatches} />
+    <Route component={NotFound} />
+  </Switch>
 )
 
 export default App
